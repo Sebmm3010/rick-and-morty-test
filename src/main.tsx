20a839a3b388e116f4c4dc19b790f2dccd4489ea
Sebastian <rockmadero@gmail.com>
2023-03-04 17:27:13 -0500

    mainLayout implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { MainLayout } from './components/layouts';
 import App from './App';
-import { AuthProvider } from './context';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* BrowserRouter */}
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <MainLayout>
+      <App />
+    </MainLayout>
   </React.StrictMode>
 );
