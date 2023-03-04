ad471d0445c26fb9f44b8df80de5e21994f6f2d4
Sebastian <rockmadero@gmail.com>
2023-03-04 17:23:16 -0500

    character card component

---
+++ src/components/ui/CharacterCard.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { Result } from '../../interfaces';
+
+export const CharacterCard: FC<Result> = (character) => {
+  const { image, name, status, species, location } = character;
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <img
+        className="rounded-t-lg w-full object-cover"
+        src={image}
+        alt={name}
+      />
+      <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {name}
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <span className="font-bold">State: </span>
+          {status}
+        </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <span className="font-bold">Species: </span>
+          {species}
+        </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <span className="font-bold">Location: </span>
+          {location.name}
+        </p>
+      </div>
+    </div>
+  );
+};
