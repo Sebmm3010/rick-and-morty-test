acf5e98209bb5bd3c53e820a7bda48230b12beaa
Sebastian <rockmadero@gmail.com>
2023-03-05 16:30:27 -0500

    no strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <MainLayout>
-      <App />
-    </MainLayout>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <MainLayout>
+    <App />
+  </MainLayout>
+  // </React.StrictMode>
 );
