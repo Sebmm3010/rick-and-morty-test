ebe72a6f488d81414b27cc8681524b0f15d0b151
Sebastian <rockmadero@gmail.com>
2023-03-05 16:32:13 -0500

    implementing context

--- src/home/pages/Capitulos.tsx
+++ src/home/pages/Capitulos.tsx
@@ -1,28 +1,32 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { HomeLayout } from '../../components/layouts';
 import { Pagination, EpisodesCard } from '../../components/ui';
 import { useRickData } from '../../hooks/usuRickData';
+import { FavoriteContext } from '../../context';
 
 export const Capitulos = () => {
   const { results, info, onNext, onPrev, handleData } = useRickData();
+  const { favorites } = useContext(FavoriteContext);
   useEffect(() => {
     handleData('https://rickandmortyapi.com/api/episode');
   }, []);
 
   return (
-    <HomeLayout>
-      {!results ? (
-        'cargando...'
-      ) : (
-        <div className="bg-gray-600 p-5 min-h-screen">
-          <Pagination info={info} onNext={onNext} onPrev={onPrev} />
-          <div className="m-auto mt-3 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 container content-center w-full">
-            {results.map((result) => (
-              <EpisodesCard episodio={result} key={result.id} />
-            ))}
+    <>
+      <HomeLayout collection={favorites.character} path="/">
+        {!results ? (
+          'cargando...'
+        ) : (
+          <div className="bg-gray-600 p-5 min-h-screen">
+            <Pagination info={info} onNext={onNext} onPrev={onPrev} />
+            <div className="m-auto mt-3 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 container content-center w-full">
+              {results.map((result) => (
+                <EpisodesCard episodio={result} key={result.id} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </HomeLayout>
+        )}
+      </HomeLayout>
+    </>
   );
 };
