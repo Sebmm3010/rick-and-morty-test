ece64bbe9360f3a400868ece0f281c295c13cb51
Sebastian <rockmadero@gmail.com>
2023-03-04 19:53:41 -0500

    capitulos page working

--- src/home/pages/Capitulos.tsx
+++ src/home/pages/Capitulos.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from 'react';
 import { HomeLayout } from '../../components/layouts';
-import React from 'react';
+import { Pagination, EpisodesCard } from '../../components/ui';
+import { useRickData } from '../../hooks/usuRickData';
 
 export const Capitulos = () => {
+  const { results, info, onNext, onPrev, handleData } = useRickData();
+  useEffect(() => {
+    handleData('https://rickandmortyapi.com/api/episode');
+  }, []);
+
   return (
     <HomeLayout>
-      <div>
-        <h1>Capitulos</h1>
-      </div>
+      {!results ? (
+        'cargando...'
+      ) : (
+        <div className="bg-gray-600 p-5 min-h-screen">
+          <Pagination info={info} onNext={onNext} onPrev={onPrev} />
+          <div className="m-auto mt-3 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 container content-center w-full">
+            {results.map((result) => (
+              <EpisodesCard episodio={result} key={result.id} />
+            ))}
+          </div>
+        </div>
+      )}
     </HomeLayout>
   );
 };
