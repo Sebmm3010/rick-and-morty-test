fd9d00c91a219ce87723f008606ff83959dbdbdf
Sebastian <rockmadero@gmail.com>
2023-03-05 16:34:59 -0500

    modal working

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,12 +1,39 @@
-import React, { useContext } from 'react';
+import { FC, useContext } from 'react';
 import { UiContext } from '../../context/ui';
+import { FavoriteContext } from '../../context/favorite';
+import { AuthContext } from '../../context/auth-context';
 
-export const Modal = () => {
-  const { setShowModal } = useContext(UiContext);
+export const Modal: FC = () => {
+  const { setShowModal, pathName, pageCollection } = useContext(UiContext);
+  const { user } = useContext(AuthContext);
+  const { setActiveElement, addCharacter, activeElement } =
+    useContext(FavoriteContext);
 
+  const handleClose = () => {
+    setShowModal(false);
+    setActiveElement(null);
+  };
+
+  const handleDelete = () => {
+    console.log('borrado');
+  };
+
+  const handleSave = () => {
+    switch (pathName) {
+      case '/':
+        addCharacter({ characterId: activeElement, userId: user._id });
+        break;
+      default:
+        console.log(pathName);
+        break;
+    }
+
+    setShowModal(false);
+    setActiveElement(null);
+  };
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[999] outline-none focus:outline-none modal">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/* content */}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-800  outline-none focus:outline-none">
@@ -30,17 +57,27 @@ export const Modal = () => {
             </div>
             {/* footer */}
             <div className="flex items-center justify-end p-6 border-t border-solid border-lime-500 rounded-b">
-              <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(false)}
-              >
-                Cerrar
-              </button>
+              {pageCollection?.includes(activeElement as number) ? (
+                <button
+                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={handleDelete}
+                >
+                  Eliminar
+                </button>
+              ) : (
+                <button
+                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={handleClose}
+                >
+                  Cerrar
+                </button>
+              )}
               <button
                 className="bg-lime-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
-                // onClick={() => setShowModal(false)}
+                onClick={handleSave}
               >
                 Guardar
               </button>
@@ -48,7 +85,7 @@ export const Modal = () => {
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div className="opacity-25 fixed inset-0 z-[999]] bg-black"></div>
     </>
   );
 };
